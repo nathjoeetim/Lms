4f699a8e714a46dd4f5ec1958259dbefb0f45302
Nathaniel <nathjoeetim@gmail.com>
2024-02-19 13:48:14 +0100

    fetched data from api

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,17 +1,16 @@
+// import React, { useState } from 'react'
+// import useAxios from './useAxios'
+// import { CurrentUser } from '@/utils/network'
+// import { UserType } from '@/utils/types'
 
-import React, { useState } from 'react'
-import useAxios from './useAxios'
-import { CurrentUser } from '@/utils/network'
-import { UserType } from '@/utils/types'
+// type Props = {}
 
-type Props = {}
+// const useUser = async (props: Props) => {
+//     const [loading, setLoading] = useState(false)
+//     const {axiosHandler} = useAxios()
+//     const res = await axiosHandler<UserType>(CurrentUser, "get", null, true)
+//     console.log(res);
+//     // triger the effect or use a state to store the user
+// }
 
-const useUser = async (props: Props) => {
-    const [loading, setLoading] = useState(false)
-    const {axiosHandler} = useAxios()
-    const res = await axiosHandler<UserType>(CurrentUser, "get", null, true)
-    console.log(res);
-    // triger the effect or use a state to store the user 
-}
-
-export default useUser
+// export default useUser
